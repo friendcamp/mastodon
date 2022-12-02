6fd41add2007189f4a10efbc1df1115ff3c97aa8
Darius Kazemi <darius.kazemi@gmail.com>
2022-12-01 21:45:32 -0800

    Updates

--- app/javascript/mastodon/features/explore/statuses.js
+++ app/javascript/mastodon/features/explore/statuses.js
@@ -37,7 +37,7 @@ class Statuses extends React.PureComponent {
   render () {
     const { isLoading, hasMore, statusIds, multiColumn } = this.props;
 
-    const emptyMessage = <FormattedMessage id='empty_column.explore_statuses' defaultMessage='Nothing is trending right now. Check back later!' />;
+    const emptyMessage = <FormattedMessage id='empty_column.explore_statuses' defaultMessage='We don\'t display trends on Friend Camp. Move along, nothing to see here.' />;
 
     return (
       <StatusList
